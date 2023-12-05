1bf98d51a934acd0a411fbae84de770532df3ca9
Charlie Brown <ncharlbr@amazon.co.uk>
2023-12-05 14:22:46 +0000

    adding put lambda and permissions

--- cdk_package/lib/cdk_package-stack.ts
+++ cdk_package/lib/cdk_package-stack.ts
@@ -3,9 +3,10 @@ import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origin from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import { Stack, StackProps } from 'aws-cdk-lib';
-
 import * as ddb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
 import { Construct } from 'constructs';
 
 export class CdkPackageStack extends Stack {
@@ -31,11 +32,33 @@ export class CdkPackageStack extends Stack {
     const distribution = new cloudfront.Distribution(this, 'epa_cloudfront', {
       defaultBehavior: {
         origin: new origin.S3Origin(bucket),
-      }});
+      }
+    });
 
     const api = new apigateway.RestApi(this, 'epa-api', {
       restApiName: 'epa-api'
     });
-    
+
+    const getFunction = new lambda.Function(this, 'Function', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'get_index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambdaHandler')),
+    });
+
+    const version = getFunction.currentVersion;
+    const alias = new lambda.Alias(this, 'GetFunctionLambdaAlias', {
+      aliasName: 'Prod',
+      version,
+    });
+
+    if (getFunction.role === null) {
+      throw new Error('Lambda function role cannot be null');
+    }
+
+    getFunction.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'))
+
+    getFunction.addEnvironment("TABLE_NAME", table.tableName)
+
+    table.grantReadWriteData(getFunction)
   }
 }
---
+++ cdk_package/lib/lambdaHandler/get_index.py
@@ -0,0 +1,7 @@
+
+Edit
+No lines are selected.
+def handler(event, content):
+    greeting = "hello world"
+    
+    return greeting 
