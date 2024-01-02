566de6e0d5b1e431e9adbc385915651b8d504edd
George Cullender <cullenge@amazon.co.uk>
2024-01-02 11:21:36 +0000

    Updated testing.

--- cdk_package/test/cdk_package.test.ts
+++ cdk_package/test/cdk_package.test.ts
@@ -29,7 +29,7 @@ test('Lambda functions created',  ()=> {
   template.hasResourceProperties('AWS::Lambda::Function', {
     Handler: 'get_index.handler'
   });
-  template.resourceCountIs('AWS::Lambda::Function', 5);
+  template.resourceCountIs('AWS::Lambda::Function', 6);
 })
 
 test('S3 bucket created', () => {
