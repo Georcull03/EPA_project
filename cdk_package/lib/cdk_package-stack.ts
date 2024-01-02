fca3f2ad0cbc850a5ad5a881e2383bad13763b74
George Cullender <cullenge@amazon.co.uk>
2024-01-02 16:00:09 +0000

    Updated IAM role and now pushing to trigger pipeline.

--- cdk_package/lib/cdk_package-stack.ts
+++ cdk_package/lib/cdk_package-stack.ts
@@ -36,7 +36,7 @@ export class CdkPackageStack extends Stack {
             }
         });
 
-        // lambda fetch interview question data
+        // lambda fetch interview question data.
         const getFunction = new lambda.Function(this, 'Function', {
             runtime: lambda.Runtime.NODEJS_18_X,
             handler: 'get_index.handler',
