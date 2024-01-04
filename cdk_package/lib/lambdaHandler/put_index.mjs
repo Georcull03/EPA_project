bb546cdf47ed38c677fa391322a4f06a59fc9eb3
George Cullender <cullenge@amazon.co.uk>
2024-01-04 09:25:30 +0000

    Updated put function in order to create records in DynamoDB table.

--- cdk_package/lib/lambdaHandler/put_index.mjs
+++ cdk_package/lib/lambdaHandler/put_index.mjs
@@ -1,10 +1,10 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
-  DynamoDBDocumentClient,
-  ScanCommand,
-  PutCommand,
-  GetCommand,
-  DeleteCommand,
+    DynamoDBDocumentClient,
+    ScanCommand,
+    PutCommand,
+    GetCommand,
+    DeleteCommand,
 } from "@aws-sdk/lib-dynamodb";
 
 // import { marshall } from "@aws-sdk/util-dynamodb";
@@ -16,45 +16,47 @@ const dynamo = DynamoDBDocumentClient.from(client);
 const tableName = "qwizgurus_interview_table";
 
 export const handler = async (event, context) => {
-  let body;
-  let statusCode = 200;
-  const headers = {
-    "Content-Type": "application/json",
-  };
-  console.log(event)
-  
-  // switch(event.routeKey)
-  // switch (event.requestContext.httpMethod)
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    console.log(event)
 
-  try {
-    switch(event.requestContext.httpMethod) {
-        case "PUT":
-            let requestJSON = JSON.parse(event.body);
-            await dynamo.send(
-              new PutCommand({
-                TableName: tableName,
-                Item: {
-                  level: requestJSON.level,
-                  question: requestJSON.question,
-                  job_role: requestJSON.job_role,
-                },
-              })
-            );
-            body = `Put item ${requestJSON.question}`;
-            break;
-          default:
-            throw new Error(`Unsupported route: "${event.routeKey}"`);
+    // switch(event.routeKey)
+    // switch (event.requestContext.httpMethod)
+
+    try {
+        switch(event.requestContext.httpMethod) {
+            case "PUT":
+                let requestJSON = JSON.parse(event.body);
+                await dynamo.send(
+                    new PutCommand({
+                        TableName: tableName,
+                        Item: {
+                            level: requestJSON.level,
+                            question: requestJSON.question,
+                            Answer: requestJSON.Answer,
+                            ManagerIC: requestJSON.ManagerIC,
+                            Role: requestJSON.Role,
+                        },
+                    })
+                );
+                body = `Put item ${requestJSON.question}`;
+                break;
+            default:
+                throw new Error(`Unsupported route: "${event.routeKey}"`);
         }
-  } catch (err) {
-    statusCode = 400;
-    body = err.message;
-  } finally {
-    body = JSON.stringify(body);
-  }
+    } catch (err) {
+        statusCode = 400;
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
 
-  return {
-    statusCode,
-    body,
-    headers,
-  };
+    return {
+        statusCode,
+        body,
+        headers,
+    };
 };
