de7eab3ac549e18a69136979dbf4a6a2df392bf4
George Cullender <cullenge@amazon.co.uk>
2024-01-08 14:02:32 +0000

    bug fix - validation error when stage name is blank.

--- cdk_package/lib/pipeline-stack.ts
+++ cdk_package/lib/pipeline-stack.ts
@@ -15,7 +15,7 @@ export class QwizPipelineStack extends cdk.Stack {
             stages: [
                 {name: "Prod", accountId: "929483735452", region: "us-west-1", isProd: false},
                 {name: "Beta", accountId: "602709950483", region: "us-west-1", isProd: false},
-                // {name: "Alpha", accountId: "911742436812", region: "us-west-1", isProd: false}.
+                // {name: "Alpha", accountId: "911742436812", region: "us-west-1", isProd: false}
             ]
         }
 
