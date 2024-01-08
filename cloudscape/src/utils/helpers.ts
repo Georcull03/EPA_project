63dfad98e89ffa4de63d319cb270933fa167433e
George Cullender <cullenge@amazon.co.uk>
2024-01-08 13:29:42 +0000

    Updated helper function to include stage name to test if that is what is causing cloudfront to not display the table through the frontend.

---
+++ cloudscape/src/utils/helpers.ts
@@ -0,0 +1,26 @@
+export function ApiPath() {
+
+    const apiDomain = 'api.cullenge.people.aws.dev'
+
+    const domainCDN = window.location.host.toLowerCase()
+
+    console.log(domainCDN)
+
+    const hasPrefix = domainCDN.includes("alpha" || "beta" || "prod")
+
+    if (hasPrefix) {
+        const usePrefix = window.location.host.toLowerCase().split('.')[0]
+        console.log('this is the prefix created: ' + usePrefix)
+        return usePrefix + apiDomain
+    }
+    else { return apiDomain }
+}
+
+export function createApiPath() {
+
+    const apiUrl = 'https://' + ApiPath()
+
+    console.log('this is the url created: ' + apiUrl)
+
+    return apiUrl
+}
