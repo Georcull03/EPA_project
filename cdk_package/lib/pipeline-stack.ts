8fc3fa0c8045d0b46aef2d48729e51ad2ab32688
George Cullender <cullenge@amazon.co.uk>
2024-01-17 08:47:13 +0000

    Changed the way that commands are inputted based upon which stage is being deployed to.

--- cdk_package/lib/pipeline-stack.ts
+++ cdk_package/lib/pipeline-stack.ts
@@ -3,7 +3,6 @@ import { ServiceStage } from './pipeline-stage'
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import {CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
-import {listOfStrings} from "aws-sdk/clients/iotfleetwise";
 
 export class QwizPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -58,20 +57,19 @@ export class QwizPipelineStack extends cdk.Stack {
                 commands: ['cd cdk_package', 'npm install', 'npm run build', 'npm run test']
             }));
 
-            let curlCommands: string[]
+            let curlFrontend: string
+            let curlApi: string
 
             if (stage.stageName == 'alpha' || 'beta') {
-                 curlCommands = [
-                    `curl -Ssf https://${stage.stageName}qwiz.cullenge.people.aws.dev/`,
-                    `curl -Ssf https://${stage.stageName}api.cullenge.people.aws.dev/question'`
-                ]
+                curlFrontend = `curl -Ssf https://${stage.stageName}qwiz.cullenge.people.aws.dev/`
+                curlApi = `curl -Ssf https://${stage.stageName}api.cullenge.people.aws.dev/question`
             } else {
-                 curlCommands = ['curl -Ssf https://qwiz.cullenge.people.aws.dev/',
-                    'curl -Ssf https://api.cullenge.people.aws.dev/question']
+                curlFrontend = 'curl -Ssf https://qwiz.cullenge.people.aws.dev/'
+                curlApi = 'curl -Ssf https://api.cullenge.people.aws.dev/question'
             }
 
             stage.addPost(new ShellStep("TestEndpoint", {
-                commands: [curlCommands.toString()]
+                commands: [curlFrontend, curlApi]
             }))
         })
     }
