da846d70da3d929a7e9f88376473147f7dadcfcd
George Cullender <cullenge@amazon.co.uk>
2024-01-19 09:29:37 +0000

    Should actually remove npm run dev from build stage of pipeline.

--- cdk_package/lib/pipeline-stack.ts
+++ cdk_package/lib/pipeline-stack.ts
@@ -41,7 +41,7 @@ export class QwizPipelineStack extends cdk.Stack {
                     'npm run test',
                     'cd ../cloudscape',
                     'npm install',
-                    'npm run build',
+                    'npm run build'
                 ],
                 primaryOutputDirectory: 'cdk_package/cdk.out'
             })
